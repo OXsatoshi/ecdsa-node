cc9fc3a86ddb449bdf4910f0943d63d8beb77b6a
OXsatoshi <afia.nabil24@gmail.com>
2024-04-13 17:39:42 +0100

    Add a signature

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,20 +1,27 @@
 import { useState } from "react";
 import server from "./server";
+import { utf8ToBytes } from "ethereum-cryptography/utils.js";
+import { secp256k1 } from "ethereum-cryptography/secp256k1.js";
+import { keccak256, keccak224, keccak384, keccak512 } from "ethereum-cryptography/keccak.js";
 
-function Transfer({ address, setBalance }) {
+
+function Transfer({ signature, setBalance }) {
   const [sendAmount, setSendAmount] = useState("");
   const [recipient, setRecipient] = useState("");
 
   const setValue = (setter) => (evt) => setter(evt.target.value);
-
+  
   async function transfer(evt) {
     evt.preventDefault();
-
+    const publicKey = "0319c4207b482da07765ae412dee0ef70e4f1210efb6c59c4d579c6b54d6b60a4e";
+    const hashedMessage=keccak256(utf8ToBytes(publicKey));
     try {
       const {
         data: { balance },
       } = await server.post(`send`, {
-        sender: address,
+        //public key , signature , hashed message
+        signature: signature,
+        publicKey:publicKey,
         amount: parseInt(sendAmount),
         recipient,
       });
@@ -40,7 +47,7 @@ function Transfer({ address, setBalance }) {
       <label>
         Recipient
         <input
-          placeholder="Type an address, for example: 0x2"
+          placeholder="Type an signature, for example: 0x2"
           value={recipient}
           onChange={setValue(setRecipient)}
         ></input>
